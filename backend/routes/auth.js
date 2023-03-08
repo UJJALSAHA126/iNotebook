c8214edbda5905b99234601823f5a069cd6f25fe
UJJAL SAHA <96197781+UJJALSAHA126@users.noreply.github.com>
2023-03-08 14:17:57 +0530

    Commit 3

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,105 +6,131 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = 'IamNil';
 
+const fetchuser = require('../middleware/fetchuser');
+
 const router = express.Router();
 
-// Create a User using : POST "/api/auth/"
-router.post("/createuser", [
-    body("name", 'Enter a valid name !').isLength({ min: 3 }),
-    body('email', 'Enter a valid email !').isEmail(),
-    body('password', 'Password in Invalid :(').isLength({ min: 5 })
-], async (req, res) => {
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
-    }
+// Route 1: Create a User using : POST "/api/auth/createuser/"
+router.post("/createuser",
+    [
+        body("name", 'Enter a valid name !').isLength({ min: 3 }),
+        body('email', 'Enter a valid email !').isEmail(),
+        body('password', 'Password in Invalid :(').isLength({ min: 5 })
+    ],
+    async (req, res) => {
 
-    try {
-
-        // Check wheather this email already exists in the database
-        let user = await User.findOne({ email: req.body.email });
-
-        if (user) {
-            return res.status(400).json({ error: 'Sorry this email is already taken' });
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
         }
 
-        const salt = await bcrypt.genSalt(10);
-        const securePass = await bcrypt.hash(req.body.password, salt);
+        try {
 
-        // Creating a new User in the database
-        user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: securePass,
-        })
+            // Check wheather this email already exists in the database
+            let user = await User.findOne({ email: req.body.email });
 
-        const data = {
-            user: {
-                id: user.id
+            if (user) {
+                return res.status(400).json({ error: 'Sorry this email is already taken' });
             }
-        }
 
-        const autToken = jwt.sign(data, JWT_SECRET);
+            const salt = await bcrypt.genSalt(10);
+            const securePass = await bcrypt.hash(req.body.password, salt);
 
-        res.json({
-            status: 'Successful',
-            message: 'User Successfuly created',
-            user: user,
-            autToken
-        });
+            // Creating a new User in the database
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: securePass,
+            })
 
-    } catch (error) {
-        res.status(500).json(error.message);
-    }
-
-});
-
-// Logging in an existing user
-router.post('/login', [
-    body('email', 'Enter a valid email !').isEmail(),
-    body('password', 'Password cannot be blank !').exists()
-], async (req, res) => {
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
-    }
-
-    const { email, password } = req.body;
-    try {
-
-        // Check wheather this email already exists in the database
-        let user = await User.findOne({ email: email });
-
-        if (!user) {
-            return res.status(400).json({ error: 'Credentials are not matced !' });
-        }
-
-        const passwordCompare = bcrypt.compareSync(password, user.password);
-        if (!passwordCompare) {
-            return res.status(400).json({ error: 'Credentials are not matced !' });
-        }
-
-        const data = {
-            user: {
-                id: user.id
+            const data = {
+                user: {
+                    id: user.id
+                }
             }
+
+            const autToken = jwt.sign(data, JWT_SECRET);
+
+            res.json({
+                status: 'Successful',
+                message: 'User Successfuly created',
+                user: user,
+                autToken
+            });
+
+        } catch (error) {
+            res.status(500).json(error.message);
         }
 
-        const authToken = jwt.sign(data, JWT_SECRET);
+    });
 
-        res.json({
-            message: 'Log in successfull',
-            userId: user.id,
-            authToken
-        });
 
+// Route 2: Logging in an existing user
+router.post('/login',
+    [
+        body('email', 'Enter a valid email !').isEmail(),
+        body('password', 'Password cannot be blank !').exists()
+    ],
+    async (req, res) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        const { email, password } = req.body;
+        try {
+
+            // Check wheather this email already exists in the database
+            let user = await User.findOne({ email: email });
+
+            if (!user) {
+                return res.status(400).json({ error: 'Credentials are not matced !' });
+            }
+
+            const passwordCompare = bcrypt.compareSync(password, user.password);
+            if (!passwordCompare) {
+                return res.status(400).json({ error: 'Credentials are not matced !' });
+            }
+
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            const authToken = jwt.sign(data, JWT_SECRET);
+
+            res.json({
+                message: 'Log in successfull',
+                userId: user.id,
+                authToken
+            });
+
+        }
+        catch (error) {
+            res.status(500).json(error.message);
+        }
+    });
+
+
+
+// Router 3: Get logged in user details
+router.post('/getuser', fetchuser,
+    async (req, res) => {
+
+        try {
+            const userId = req.user.id;
+            const user = await User.findById(userId).select('-password');
+
+            res.json(user);
+
+        }
+        catch (error) {
+            res.status(500).json(error.message);
+        }
     }
-    catch (error) {
-        res.status(500).json(error.message);
-    }
-
-});
+)
 
 module.exports = router;
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,16 +1,125 @@
-const express = require('express');
-const User = require('../models/User');
+const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
+const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
+
+// Route 1: Get all the notes
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+});
+
+// Route 2: Add a new note using post
+router.post(
+    "/addnote",
+    fetchuser,
+    [
+        body("title", "Enter a valid title !").isLength({ min: 3 }),
+        body("description", "Enter a valid description !").isLength({ min: 5 }),
+    ],
+
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const { title, description, tag } = req.body;
+
+            const note = new Note({
+                title,
+                description,
+                tag,
+                user: req.user.id,
+            });
+
+            const savedNote = await note.save();
+
+            res.json(savedNote);
+
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
+    }
+);
+
+// Route 3: Update an existing note
+router.put(
+    "/updatenote/:id",
+    fetchuser,
+    [
+        body("title", "Enter a valid title !").isLength({ min: 3 }),
+        body("description", "Enter a valid description !").isLength({ min: 5 }),
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const { title, description, tag } = req.body;
+
+            const newNote = {};
+            if (title) newNote.title = title;
+            if (description) newNote.description = description;
+            if (tag) newNote.tag = tag;
+
+            // Find the note and update
+
+            let note = await Note.findByIdAndUpdate(req.params.id);
+            if (!note) return res.status(404).send("Note note found !!");
+
+            if (req.user.id !== note.user.toString()) {
+                return res.status(404).send("Access Denied !!");
+            }
+
+            note = await Note.findByIdAndUpdate(
+                req.params.id,
+                { $set: newNote },
+                { new: true }
+            );
+            res.json({ note });
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
+    }
+);
 
 
-router.get('/', (req, res) => {
-    res.send('Notes');
 
+// Route 4: Delete an existing note
+router.delete(
+    "/deletenote/:id",
+    fetchuser,
+    async (req, res) => {
+        try {
+            
+            // Find the note and delete
 
-    console.log('Req', req.body);
-})
+            let note = await Note.findByIdAndUpdate(req.params.id);
+            if (!note) return res.status(404).send("Note note found !!");
 
+            if (req.user.id !== note.user.toString()) {
+                return res.status(404).send("Access Denied !!");
+            }
 
+            note = await Note.findByIdAndDelete(req.params.id);
+            res.json({ success: "Note has been deleted successfully", note: note });
 
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
+    }
+);
+
+router.get("/", (req, res) => {
+    res.send("Notes");
+
+    console.log("Req", req.body);
+});
 
 module.exports = router;
