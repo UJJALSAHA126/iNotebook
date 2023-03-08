d549d0827f75e43773789328805b1795023fa638
UJJAL SAHA <96197781+UJJALSAHA126@users.noreply.github.com>
2023-03-08 23:07:39 +0530

    Commit 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+
+import { Switch } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar></Navbar>
+        <Home />
+        <Switch>
+
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Notes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Notes() {
+    return (
+        <>
+            <span>Notes</span>
+        </>
+    )
+}
+
+export default Notes
